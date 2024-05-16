c1116436d3fcdce607fbfa3a0321f1155d1e1779
irhl <irhl@users.noreply.github.com>
2024-05-17 01:06:57 +0800

    ✒️

--- .config/emacs/themes/untitled-theme.el
+++ .config/emacs/themes/untitled-theme.el
@@ -1,3 +1,7 @@
+(unless (>= emacs-major-version 29)
+  (error
+    "hola payaso fuck you no fudgee barr for you motherfucker"))
+
 (deftheme untitled
   "untitled by irhl")
 
@@ -26,30 +30,33 @@
  '(rainbow-delimiters-depth-11-face ((t (:foreground "#c79d83"))))
  '(rainbow-delimiters-depth-12-face ((t (:foreground "#c79d83"))))
 
- '(font-lock-builtin-face           ((t (:foreground "#5e5958"))))
+ '(font-lock-builtin-face           ((t (:foreground "#918b8b"))))
  '(font-lock-comment-face           ((t (:foreground "#d2cbc6"))))
  '(font-lock-comment-delimiter-face ((t (:foreground "#d2cbc6"))))
- '(font-lock-doc-face               ((t (:foreground "#5e5958"))))
- '(font-lock-constant-face          ((t (:foreground "#918b8b"))))
- '(font-lock-function-name-face     ((t (:foreground "#5e5958"))))
- '(font-lock-keyword-face           ((t (:foreground "#5e5958"))))
+ '(font-lock-doc-face               ((t (:foreground "#d2cbc6"))))
+ '(font-lock-constant-face          ((t (:foreground "#9cd2bb"))))
+ '(font-lock-function-name-face     ((t (:foreground "#bea292"))))
+ '(font-lock-keyword-face           ((t (:foreground "#cfbbb0"))))
+ '(font-lock-doc-string-face        ((t (:foreground "#d2cbc6"))))
  '(font-lock-string-face            ((t (:foreground "#5e5958"))))
  '(font-lock-type-face              ((t (:foreground "#5e5958"))))
- '(font-lock-variable-name-face     ((t (:foreground "#5e5958"))))
+ '(font-lock-variable-name-face     ((t (:foreground "#b3a7a9"))))
+ '(font-lock-preprocessor           ((t (:foreground "#b3a7a9"))))
  '(font-lock-warning-face           ((t (:foreground "#5e5958"))))
 
  '(hl-line                          ((t (:background "#F5EFE8"))))
  '(region                           ((t (:background "#f0e4dd"))))
  '(show-paren-match                 ((t (:background "#f5cac3"))))
-
  '(vertical-border                  ((t (:foreground "#f3ebeb"))))
 
- '(default            ((t (:foreground "#5e5958" :background "#fcf5ee"))))
- '(warning            ((t (:foreground "#77706e" :weight bold))))
-
- '(mode-line          ((t (:foreground "#7c7773" :background "#e2e8f2"))))
- '(mode-line-inactive ((t (:foreground "#9d9ea0" :background "#dfcfc5"))))
-
+ '(mode-line                        ((t (:foreground "#7c7773"
+                                         :background "#f4e7e1"))))
+ '(mode-line-inactive               ((t (:foreground "#9d9ea0"
+                                         :background "#dfcfc5"))))
+ '(default                          ((t (:foreground "#5e5958"
+                                         :background "#fcf5ee"))))
+ '(warning                          ((t (:foreground "#5e5958"
+                                         :weight bold))))
  ;; end of highlight groups
  )
 
